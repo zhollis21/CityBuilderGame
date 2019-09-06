e62d5a3f283803f9b930b1febe6fe978c75e8223
Zach Hollis <zach.hollis@dpra.com>
2019-09-06 14:48:31 -0400

    Added a Map, UI, and moved the sprites into a more detailed folder system

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+    private const int MapRadius = 5;
+    private Dictionary<Vector2, GameObject> gameMap = new Dictionary<Vector2, GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int row = -MapRadius; row <= MapRadius; row++)
+        {
+            for (int col = -MapRadius; col <= MapRadius; col++)
+            {
+                gameMap.Add(new Vector2(row, col), null);
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
