e4b5bbcc61cfd4fd63ce672de2788bcf6e06db36
Zach Hollis <zach.hollis@dpra.com>
2019-09-06 18:49:40 -0400

    Finished the Map class
    Seperated the prefabs into Buildings and Nature
    
    Map -
    * Added grass as the default tile background
    * The sorting order now increaments by building added
    * The map is now the parent for all tiles that belong to it
    
    Added a grass sprite

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -5,26 +5,36 @@ using UnityEngine;
 public class Map : MonoBehaviour
 {
     public GameObject FarmPrefab;
+    public GameObject GrassPrefab;
     public GameObject HousePrefab;
     public GameObject TownCenter;
     public GameObject WoodcutterPrefab;
 
-    private const int MapRadius = 5;
+    private const int MAP_RADIUS = 5;
+    private int buildingNumber = 1;
     private Dictionary<Vector2, GameObject> gameMap = new Dictionary<Vector2, GameObject>();
     private GameObject pendingBuilding;
+    private SpriteRenderer pendingBuildingRenderer;
+    private Color validPlacementColor = new Color(.5f, 1, .5f, 0.75f);
+    private Color invalidPlacementColor = new Color(1, 0.5f, 0.5f, 0.75f);
 
     // Start is called before the first frame update
     void Start()
     {
-        for (int row = -MapRadius; row <= MapRadius; row++)
+        for (int col = -MAP_RADIUS; col <= MAP_RADIUS; col++)
         {
-            for (int col = -MapRadius; col <= MapRadius; col++)
+            for (int row = -MAP_RADIUS; row <= MAP_RADIUS; row++)
             {
-                gameMap.Add(new Vector2(row, col), null);
+                Vector2 position = new Vector2(col, row);
+
+                GameObject grassSpot = Instantiate(GrassPrefab, transform);
+                grassSpot.transform.position = position;
+
+                gameMap.Add(position, null);
             }
         }
 
-        gameMap[Vector2.zero] = Instantiate(TownCenter);
+        gameMap[Vector2.zero] = Instantiate(TownCenter, transform);
     }
 
     // Update is called once per frame
@@ -33,28 +43,63 @@ public class Map : MonoBehaviour
         if (pendingBuilding == null)
             return;
 
+        bool canPlaceBuilding;
+
         Vector2 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        pendingBuilding.transform.position = mouseWorldPosition;
+        Vector2 gridPosition = new Vector2(Mathf.Round(mouseWorldPosition.x), Mathf.Round(mouseWorldPosition.y));
+        pendingBuilding.transform.position = gridPosition;
+
+        if (gameMap.ContainsKey(gridPosition) && gameMap[gridPosition] == null)
+        {
+            canPlaceBuilding = true;
+
+            pendingBuildingRenderer.color = validPlacementColor;
+        }
+        else
+        {
+            canPlaceBuilding = false;
+
+            pendingBuildingRenderer.color = invalidPlacementColor;
+        }
 
         if (Input.GetMouseButtonDown(0))
         {
-            Debug.Log(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+            if (canPlaceBuilding)
+            {
+                pendingBuildingRenderer.color = Color.white;
+                gameMap[gridPosition] = pendingBuilding;
+            }
+            else
+            {
+                Destroy(pendingBuilding.gameObject);
+            }
+
             pendingBuilding = null;
+            pendingBuildingRenderer = null;
         }
+    }
+
+    public void CreatePendingBuilding(GameObject obj)
+    {
+        pendingBuilding = Instantiate(obj, transform);
+
+        pendingBuildingRenderer = pendingBuilding.GetComponent<SpriteRenderer>();
+
+        pendingBuildingRenderer.sortingOrder = buildingNumber++;
     }
 
     public void AddFarm()
     {
-        pendingBuilding = Instantiate(FarmPrefab);        
+        CreatePendingBuilding(FarmPrefab);
     }
 
     public void AddHouse()
     {
-        pendingBuilding = Instantiate(HousePrefab);
+        CreatePendingBuilding(HousePrefab);
     }
 
     public void AddWoodcutter()
     {
-        pendingBuilding = Instantiate(WoodcutterPrefab);
+        CreatePendingBuilding(WoodcutterPrefab);
     }
 }
