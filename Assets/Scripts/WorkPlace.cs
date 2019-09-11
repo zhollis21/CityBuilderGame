0a96f11ac92e46463cf086ff372867761c3e2b61
Zach Hollis <zach.hollis@dpra.com>
2019-09-11 18:47:04 -0400

    Replaced the Farm and Woodcutter scripts with the Workplace Script

--- Assets/Scripts/WorkPlace.cs
+++ Assets/Scripts/WorkPlace.cs
@@ -5,41 +5,65 @@ using UnityEngine.UI;
 
 public class WorkPlace : MonoBehaviour
 {
-    public Slider slider;
+    public enum WorkType { Farm, Woodcutter}
 
-    protected int WorkplaceLevel = 1;
-    protected int productionPerLevel = 1;
-    protected int cycleTime = 3;
-    protected float productionTimer = 0;
-    protected List<Person> workers = new List<Person>();
-    protected int maxWorkers = 5;
+    public Slider ProgressIndicator;
+    public WorkType WorkplaceType;
+    public int WorkplaceLevel = 1;
+    public int ProductionPerLevel = 1;
+    public int CycleTime = 3;
+    public int MaxWorkers = 5;
+
+    private float productionTimer = 0;
+    private List<Person> workers = new List<Person>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        BackfillPositions();
+
+        int produce = GetAmountProduced();
+        if (produce > 0)
+        {
+            if (WorkplaceType == WorkType.Farm)
+                GameManager.instance.FoodCount += produce;
+            else if (WorkplaceType == WorkType.Woodcutter)
+                GameManager.instance.WoodCount += produce;
+        }
+    }
 
     public void Resign(Person p)
     {
         workers.Remove(p);
     }
 
-    protected int ProductionAmount()
+    protected int GetAmountProduced()
     {
         int produceAmount = 0;
-        float workerPercentage = workers.Count / (float)maxWorkers;
+        float workerPercentage = workers.Count / (float)MaxWorkers;
         productionTimer += Time.deltaTime * workerPercentage;
 
-        if (productionTimer > cycleTime)
+        if (productionTimer > CycleTime)
         {
-            produceAmount = productionPerLevel * WorkplaceLevel;
+            produceAmount = ProductionPerLevel * WorkplaceLevel;
 
             productionTimer = 0;
         }
 
-        slider.normalizedValue = productionTimer / cycleTime;
+        ProgressIndicator.normalizedValue = productionTimer / CycleTime;
 
         return produceAmount;
     }
 
     protected void BackfillPositions()
     {
-        while (workers.Count < maxWorkers)
+        while (workers.Count < MaxWorkers)
         {
             var newWorker = GameManager.instance.GetAvailablePerson();
 
